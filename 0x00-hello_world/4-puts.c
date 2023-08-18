18235e95750dc18b71210514664f91b77df009d1
Thabet05 <thabetkhelkhal@gmail.com>
2023-08-18 07:34:59 -0700

    9th commit today

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
-int  main (void)
+int  main(void)
 {
-puts("\"Programming is like buildiiing a multilingual puzz
-le"); 
+puts("\"Programming is like buildiiing a multilingual puzzle"); 
 return (0);
 }
