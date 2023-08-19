2dc9324f3719ab9b63568e0a1ae1aafbc389680d
Thabet05 <thabetkhelkhal@gmail.com>
2023-08-19 07:58:24 -0700

    my 5th commit today

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/*
+ * main - program that prints a line program that prints Using the function puts 
+ * the program should return 0
+ */
 int  main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
