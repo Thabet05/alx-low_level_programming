b10a62e2eb6e9ee4882f36b17437c01205aeed15
Thabet05 <thabetkhelkhal@gmail.com>
2023-08-22 07:49:47 -0700

    print reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_rev - a function that prints a string, in reverse
+ * followed by a new line.
+ * @s:tring to print in revese
+ */
+void print_rev(char *s)
+{
+	while(s == '\0')
+	{
+		s--;
+		putchar(*s);
+	}
+	putchar('\n');
+
