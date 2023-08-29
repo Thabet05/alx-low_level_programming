0cf0e521e54aa02e9f4e55e5999dfce5123f722b
Thabet05 <thabetkhelkhal@gmail.com>
2023-08-28 18:05:16 -0700

    Mon 28 Aug 2023 06:05:16 PM PDT

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_chessboard -  a function that prints the chessboard
+ * @a: input square matrix
+ * Return: 0
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i;
+
+	int j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+			printf("%d", a[i][j]);
+		printf("\n");
+	}
+}
