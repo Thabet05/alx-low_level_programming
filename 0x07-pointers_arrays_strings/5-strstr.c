be314714c3614cea8087fe4fa8205757a4fcee35
Thabet05 <thabetkhelkhal@gmail.com>
2023-08-28 17:06:14 -0700

    Mon 28 Aug 2023 05:06:14 PM PDT

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,35 @@
+nclude "main.h"
+/**
+ * _strstr - a function that locates a substring
+ * @haystack: the string
+ * @needle:the substring
+ * Return: pointer to the begining of the located substring
+ */
+char *_strstr(char *haystack, char *needle)
+{
+		int i, j, k;
+
+		for (i = 0; haystack[i] != 0; i++)
+		{
+			for (j = 0; needle[j] != 0; j++)
+			{
+				k = i;
+				if (needle[j] == haystack[k])
+				{
+					while (needle[j] == haystack[k])
+					{
+						k++;
+						j++;
+					}
+					if (needle[j] == 0)
+						return (haystack + i);
+				}
+				else
+				{
+					break;
+					j = 0;
+				}
+			}
+		}
+		return (0);
+}
