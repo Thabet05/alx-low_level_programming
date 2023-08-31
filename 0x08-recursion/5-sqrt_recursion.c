3bd3de16520d645625fd7f03a4432865d1a7b1e3
Thabet05 <thabetkhelkhal@gmail.com>
2023-08-30 18:06:05 -0700

    Wed 30 Aug 2023 06:06:05 PM PDT

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - a function that returns the natural square root of a number
+ * @n: the input number
+ * Return: natural square of n
+ */
+int _sqrt_recursion(int n)
+{
+	if 
+		return (-1);
+
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,7 @@
+#include "main.h"
+/**
+ * is_prime_number - a function that returns 1 if the input integer
+ * is a prime number, otherwise return 0
+ * @n: input number
+ * Return : 1
+ */
