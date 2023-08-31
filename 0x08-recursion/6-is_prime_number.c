cb5f5e6c77553e6ef584df84c07f514d26848a20
Thabet05 <thabetkhelkhal@gmail.com>
2023-08-30 18:30:40 -0700

    Wed 30 Aug 2023 06:30:40 PM PDT

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -3,5 +3,29 @@
  * is_prime_number - a function that returns 1 if the input integer
  * is a prime number, otherwise return 0
  * @n: input number
- * Return : 1
+ * Return : 1 if n prime, 0 otherwise
  */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	if (n == 2)
+		return (1);
+	if (n % 2 == 0)
+		return (0);
+	return prime_numbers(n, 3)
+}
+/**
+ * prime_numbers - secondary function
+ * @n: input number
+ * @m: intermediary input
+ * Return: 1 if n prime, 0 otherwise
+ */
+int prime_numbers(int n, int m)
+{
+	if (m >= n)
+		return (1);
+	if (n % m == 0)
+		return (0);
+	return (prime_numbers(n, m + 2));
+}
