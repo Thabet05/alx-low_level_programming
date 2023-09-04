aff7e28c9431323a12f22d94cdcdedc9e4d3bcca
Thabet05 <thabetkhelkhal@gmail.com>
2023-09-04 03:49:24 -0700

    Mon 04 Sep 2023 03:49:24 AM PDT

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
- * main - prints _putchar, followed by a new line
+ * main - entry point
+ * prints _putchar, followed by a new line
  * @c: the input string
  * Return: 0
  */
