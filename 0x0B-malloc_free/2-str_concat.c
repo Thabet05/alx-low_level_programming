16ffb554772a62d86e2d02a158f7aa98b2cab5a1
Thabet05 <thabetkhelkhal@gmail.com>
2023-09-05 14:21:27 -0700

    Tue 05 Sep 2023 02:21:27 PM PDT

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,9 +16,9 @@ char *str_concat(char *s1, char *s2)
 		s2 = "";
 	l1 = 0;
 	l2 = 0;
-	while (s1[l1] != 0)
+	while (s1[l1] != '\0')
 		l1++;
-	while (s2[l2] != 0)
+	while (s2[l2] != '\0')
 		l2++;
 	l2++;
 	p = malloc(sizeof(char) * (l1 + l2));
