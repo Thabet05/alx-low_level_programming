a8f798d3d9a463eb3f3a5903aaaaf6c9a5f34c36
Thabet05 <thabetkhelkhal@gmail.com>
2023-09-05 06:52:51 -0700

    Tue 05 Sep 2023 06:52:51 AM PDT

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0 || q == 0)
 		return (0);
 	for (index = 0; index < size; index++)
-		q[index] = T;
+		q[index] = c;
 
 	return (q);
 }
