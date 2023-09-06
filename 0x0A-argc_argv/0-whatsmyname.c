48914497fd9dbfddeb4a2901351373d0c830ca39
Thabet05 <thabetkhelkhal@gmail.com>
2023-09-06 05:16:47 -0700

    Wed 06 Sep 2023 05:16:47 AM PDT

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - a program that prints its name, followed by a new line
+ * @argc: the size of the array argv
+ * @argv: the array 
+ * Return: 0
+ */
+int main(int argc, char argv[])
+{
+	printf("%s\n", argv[argc - 1]);
+	return (0);
+}
