a4dfd8a61f66d98ff1c160fee5d12a14ad9afe27
Thabet05 <thabetkhelkhal@gmail.com>
2023-09-06 07:27:17 -0700

    Wed 06 Sep 2023 07:27:17 AM PDT

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -7,15 +7,16 @@
  */
 int main(int argc, char *argv[])
 {
-	int num1, num2;
-
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]);
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	printf("%d\n", num1 * num2);
+	int num1, num2, res;
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	res = num1 * num2;
+	printf("%d\n", res);
 	return (0);
 }
