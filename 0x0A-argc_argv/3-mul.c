e62a69f5d75d4e16200e11675f50d3bfa7e68966
Thabet05 <thabetkhelkhal@gmail.com>
2023-09-06 07:31:11 -0700

    Wed 06 Sep 2023 07:31:11 AM PDT

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -3,20 +3,20 @@
  * main - a program that prints the multiplication of 2 numbers
  * @argc: the size of the array argv
  * @argv: the array
- * Return: 0
+ * Return: 0 or 1
  */
 int main(int argc, char *argv[])
 {
-	if (argc != 3)
-	{
-		printf("Error\n");
-		return (1);
-	}
 	int num1, num2, res;
 
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[2]);
 	res = num1 * num2;
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
 	printf("%d\n", res);
 	return (0);
 }
