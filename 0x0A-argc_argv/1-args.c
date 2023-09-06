cbd61ae58aa6751865a9620d480fc3606f841717
Thabet05 <thabetkhelkhal@gmail.com>
2023-09-06 05:53:19 -0700

    Wed 06 Sep 2023 05:53:19 AM PDT

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - a program that prints the number of arguments followed by a new line
+ * @argc: the size of the array argv
+ * @argv: the array
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	_putchar("%d\n", argc - 1);
+	return (0);
+}
