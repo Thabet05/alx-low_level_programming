26013a6505634ca63997a935c23baa889799a73e
Thabet05 <thabetkhelkhal@gmail.com>
2023-09-07 09:40:34 -0700

    Thu 07 Sep 2023 09:40:34 AM PDT

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -8,7 +8,24 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char s3[];
-	int i;
+	char *s3;
+	int i, j, l1;
 
-	for (i = 0; i < 
+	for (l1 ==0; s1 != 0; l1++)
+		;
+	s3 = malloc(siezeof(char) * (l1 + n + 1));
+	if s3 == 0
+		return (0);
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		s3[i] = s1[i];
+	}
+	i++;
+	for (j = 0; s2[j] < n; j++, i++)
+	{
+		s3[i] = s2[j];
+	}
+	i++;
+	s3[i] = '\0';
+	return (s3);
+}	
