00b46005bb4e52e18e1576154e5604db442477ca
Thabet05 <thabetkhelkhal@gmail.com>
2023-09-08 14:44:59 -0700

    Fri 08 Sep 2023 02:44:59 PM PDT

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -4,15 +4,21 @@
  * @s1: the first input string
  * @s2: the second input string
  * @n: the first n bytes of s2
- * Retuen: pointer or NULL
+ * Retuen: pointer to the concatenated string
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *s3;
 	int i, j, l1;
 
-	for (l1 ==0; s1 != 0; l1++)
+	if (s1 == 0)
 		;
+	for (l1 == 0; s1[l1] != '\0'; l1++)
+		;
+	for (l2 == 0; s2[l2] != '\0'; l2++)
+		;
+	if (n >= l2)
+		n = l2;
 	s3 = malloc(siezeof(char) * (l1 + n + 1));
 	if s3 == 0
 		return (0);
@@ -20,11 +26,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		s3[i] = s1[i];
 	}
-	for (j = 0; s2[j] < n; j++, i++)
+	for (j = 0; s2[j] < n; j++)
 	{
 		s3[i] = s2[j];
+		i++;
 	}
-	i++;
 	s3[i] = '\0';
 	return (s3);
-}	
+}
