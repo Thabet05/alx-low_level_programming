20da26f8da841159d0e1e3e382c802cb6cd4868f
Thabet05 <thabetkhelkhal@gmail.com>
2023-09-08 15:12:36 -0700

    Fri 08 Sep 2023 03:12:36 PM PDT

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,19 @@
+nclude "main.h"
+/**
+ * _calloc - allocate memory for an array
+ * @nmemb: number of elements
+ * @size: size of an element
+ * Return: a pointer to allocating memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *q;
+	
+	if (nmemb == 0 || size == 0)
+		return (0);
+	q = malloc(nmemb * size);
+	if (q == 0)
+		return (0);
+	memset(q, 0, nmemb * size);
+	return (q);
+}
