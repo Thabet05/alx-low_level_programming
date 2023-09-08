f8be35ef75b7bd97204e8c8e21b2fc9af11cb6fc
Thabet05 <thabetkhelkhal@gmail.com>
2023-09-08 15:09:07 -0700

    Fri 08 Sep 2023 03:09:07 PM PDT

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib>
 /**
  * string_nconcat - function that concatenates two strings
  * @s1: the first input string
@@ -9,26 +10,24 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *s;
-	int i, j, l1, l2;
+	unsigned int i = 0, j = 0, l1 = 0, l2 = 0;
 
-	if (s1 == 0)
+	for (l1 = 0; s1[l1] != '\0'; l1++)
 		;
 	for (l2 = 0; s2[l2] != '\0'; l2++)
 		;
-	if (n >= l2)
-		n = l2;
-	s = malloc(siezeof(char) * (l1 + n + 1));
-	if (s == 0)
-		return (0);
+	if (n < l2)
+		s = malloc(siezeof(char) * (l1 + n + 1));
+	else
+		s = malloc(siezeof(char) * (l1 + l2 + 1));
+	if (!s)
+		return (NULL);
 	for (i = 0; s1[i] != 0; i++)
-	{
 		s[i] = s1[i];
-	}
-	for (j = 0; s2[j] < n; j++)
-	{
-		s[i] = s2[j];
-		i++;
-	}
-	s[i] = 0;
+	while (n < l2 && i < (l1 + n))
+		s[i++] = s2[j++];
+	while (n < l2 && i < (l1 + l2))
+		s[i++] = s2[j++];
+	s[i] = '\0';
 	return (s);
 }
