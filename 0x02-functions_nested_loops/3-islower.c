036ebbbd5c5df01d4c35acb8a3c86e78b709f5f1
Thabet05 <thabetkhelkhal@gmail.com>
2023-09-10 13:16:54 +0100

    commited

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
  * _islower - a function that checks for lowercase character
+ * @c: the input ascii
  * Return: 0 or 1
  */
 int _islower(int c)
---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _isalpha - a function that checks for alphabetic character
+ * @c: the input ascii  
+ * Return: 0 or 1  
+ */
+int _isalpha(int c)
+{
+	if ((c > 64 && c < 91) || (c > 96 && c < 123))
+		return (1);
+	return (0);
+}
