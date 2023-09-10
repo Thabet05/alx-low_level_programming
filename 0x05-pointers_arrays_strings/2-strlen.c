2002e870d4016402df1b69ba216efc6ad9907618
Thabet05 <thabetkhelkhal@gmail.com>
2023-09-10 07:41:33 -0700

    Sun 10 Sep 2023 07:41:33 AM PDT

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -5,25 +5,13 @@
  * Return: length
  */
 int _strlen(char *s)
-<<<<<<< HEAD
 {
-	int i;
+	int i = 0;
 
-        i = 0;
-        
-        while(*s != '\0')
-=======
-{       
-        int i;
-
-i = 0;
-
-while(*s != '\0')
->>>>>>> 8dfb00aba60e11f1c43b178e07e990ad57d188c3
-        {
-                i++;
-                s++;
-        }
-
-        return(i);
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+	return (i);
 }
