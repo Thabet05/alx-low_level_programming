744256fe48acfad387e6543a7e8141f2c465c02d
Thabet05 <thabetkhelkhal@gmail.com>
2023-09-10 12:29:18 +0100

    done

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _islower - a function that checks for lowercase character
+ * Return: 0 or 1
+ */
+int _islower(int c)
+{
+	if (c > 96 && c < 123)
+		return (1);
+	return (0);
+}
