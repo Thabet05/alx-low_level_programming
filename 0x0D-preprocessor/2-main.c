129254a632e53ae5f80f36a62be68ac59e61f77f
Thabet05 <thabetkhelkhal@gmail.com>
2023-09-11 13:27:34 -0700

    Mon 11 Sep 2023 01:27:34 PM PDT

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,17 +1,11 @@
 #include "main.h"
 /**
- * - program that prints the name of the file
+ * main - program that prints the name of the file
  * it was compiled from, followed by a new line
  * Return: 0
  */
 int main(void)
 {
-	char* name = __FILE__;
-
-	for (int i = 0; name[i] != '\0'; i++)
-	{
-		_putchar(name[i]);
-	}
-	_putchar('\n');
-	return 0;
+	PRINT_STRING;
+	return (0);
 }
