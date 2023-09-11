2bcc6a9066d5dad25843d14e689764fb9dfda771
Thabet05 <thabetkhelkhal@gmail.com>
2023-09-11 13:46:47 -0700

    Mon 11 Sep 2023 01:46:47 PM PDT

--- 0x0D-preprocessor/main.h
+++ 0x0D-preprocessor/main.h
@@ -2,9 +2,9 @@
 #define MAIN_H
 #define PRINT printf("%s\n", __FILE__)
 
-#include<sidlib.h>
-#include<sidio.h>
-#include<string.h>
+#include <sidlib.h>
+#include <sidio.h>
+#include <string.h>
 
 int _putchar(char c);
 
