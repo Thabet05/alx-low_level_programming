24b70bfdd5354beeda85349577590dc12903c702
Thabet05 <thabetkhelkhal@gmail.com>
2023-09-11 13:43:07 -0700

    Mon 11 Sep 2023 01:43:07 PM PDT

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,10 @@
 #include "main.h"
 /**
  * main - program that prints the name of the file
- * it was compiled from, followed by a new line
  * Return: 0
  */
 int main(void)
 {
-	PRINT_STRING;
+	PRINT;
 	return (0);
 }
--- 0x0D-preprocessor/main.h
+++ 0x0D-preprocessor/main.h
@@ -1,11 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
-#define PRINT_STRING printf("%s\n", __FILE__)
+#define PRINT printf("%s\n", __FILE__)
+
 #include<sidlib.h>
 #include<sidio.h>
 #include<string.h>
 
 int _putchar(char c);
 
-
-#endif
+#endif MAIN_H
