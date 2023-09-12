cfe4c0e1ab66f138f5c86ed859c6c3a2104f13b2
Thabet05 <thabetkhelkhal@gmail.com>
2023-09-12 15:57:56 +0100

    6th commit

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -7,7 +7,8 @@
  * @age:age of the dog
  * @owner: owner of the dog
  * Return: void
- */void init_dog(struct dog *d, char *name, float age, char *owner)
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != 0)
 	{
