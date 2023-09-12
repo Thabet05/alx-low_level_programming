b9cd24e6346c2cb6411bd16b7ee24d8b9d88d28b
Thabet05 <thabetkhelkhal@gmail.com>
2023-09-12 23:06:20 +0100

    commit 16

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,10 @@
 #ifndef DOG_H
 #define DOG_H
-// The contents of the dog.h file go here
 /**
  * struct dog - the structure of a dog
  * @name: the name of the dog
  * @age: the ageof the dog
  * @owner: the owner of the dog
- * Description: dog structure
  */
 typedef struct dog
 {
@@ -19,4 +17,4 @@ void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
-#endif//DOG_H
+#endif
