34c8775178a29a7f1cd01b9280f3d153a8e35107
Thabet05 <thabetkhelkhal@gmail.com>
2023-09-12 15:40:11 +0100

    second commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,11 @@
-#include <stdio.h>
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - the structure of a dog
+ * @name: the name of the dog
+ * @age: the ageof the dog
+ * @owner: the owner of the dog
+ */
 struct dog
 {
 	char *name;
