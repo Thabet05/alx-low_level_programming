bb865d8d63f9681310343ffab8aa39e5bea1bc75
Thabet05 <thabetkhelkhal@gmail.com>
2023-09-13 16:55:01 -0700

    Wed 13 Sep 2023 04:55:01 PM PDT

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_name - a function that prints a name
+ * @name: the input string
+ * @f: function pointer
+ * Return: 0
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (f && name)
+		(*f)(name);
+}
