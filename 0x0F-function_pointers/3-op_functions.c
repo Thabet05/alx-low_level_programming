6517a774aea2a52da2cfd1d422ab3088774689ef
Thabet05 <thabetkhelkhal@gmail.com>
2023-09-13 17:37:18 -0700

    Wed 13 Sep 2023 05:37:18 PM PDT

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+ * op_add - a function that returns the sum
+ * @a: first input number
+ * @b: second input number
+ * Return: the sum
+ */
+int op_add(int a, int b)
+{
+		return (a + b);
+}
+/**
+ * op_mul - a function that returns the multiplication
+ * @a: first input number
+ * @b: second input number
+ * Return: the multiplication
+ */
+int op_mul(int a, int b)
+{
+		return (a * b);
+}
+/**
+ * op_sub - a function that returns the sub
+ * @a: first input number
+ * @b: second input number
+ * Return: the sub
+ */
+int op_sub(int a, int b)
+{
+		return (a - b);
+}
+/**
+ * op_div - a function that returns the div
+ * @a: first input number
+ * @b: second input number
+ * Return: the div
+ */
+int op_div(int a, int b)
+{
+			return (a / b);
+}
+/**
+ * op_mod - a function that returns the modular
+ * @a: first input number
+ * @b: second input number
+ * Return: the modulo
+ */
+int op_mod(int a, int b)
+{
+			return (a % b);
+}
