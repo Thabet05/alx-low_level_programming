04aea1c76452d88bfa639b67d66a2c5554c56fa6
Thabet05 <thabetkhelkhal@gmail.com>
2023-09-22 07:57:54 -0700

    commit num 7

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,7 +1,10 @@
 #include "lists.h"
 /**
- *
+ *print_list - function return the number of elements of list_t
+ *@h: the input list chainne
+ *Return: the number of nodes
  */
-
+size_t list_len(const list_t *h)
 {
+	
 }
