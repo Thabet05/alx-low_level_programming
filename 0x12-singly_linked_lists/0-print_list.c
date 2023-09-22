5cd36b18b6984042c992db7798eb2a2e52e76068
Thabet05 <thabetkhelkhal@gmail.com>
2023-09-22 07:34:04 -0700

    commitnum 3

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,6 +2,16 @@
 /**
  *
  */
-
+size_t print_list(const list_t *h)
 {
+	size_t numberofnodes;
+	const list_t *head;
+
+	head = h;
+	if (head->str == 0)
+		printf("[0] (nil)\n");
+	printf("[%d] (%s)\n", head->len, head->str);
+	head = head->next;
+	numberofnodes++;
+
 }
