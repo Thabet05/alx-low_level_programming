141fede9e0f2a044352c29467723ff622de6a9a4
Thabet05 <thabetkhelkhal@gmail.com>
2023-09-24 06:35:45 -0700

    Sun 24 Sep 2023 06:35:45 AM PDT

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strncat - a function that concatenates two strings
+ * @src: input value
+ * @dest: input value
+ * @n: tne number of bytes
+ * Return: void
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+        int i;
+
+        int j;
+
+        i = 0;
+        while (dest[i] != '\0')
+        {
+                i++;
+        }
+        j = 0;
+        while (src[j] != '\0')
+        {
+                dest[i] = src[j];
+                i++;
+                j++;
+        }
+        dest[i] = '\0';
+        return (dest);
+}
