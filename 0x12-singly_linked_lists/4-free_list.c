a86fc1053072e2fe185e3b0896963f61c5bd4c43
Thabet05 <thabetkhelkhal@gmail.com>
2023-09-23 17:35:23 -0700

    Sat 23 Sep 2023 05:35:23 PM PDT

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,7 +1,20 @@
 #include "lists.h"
 /**
- *
+ * free_list - free a list_t
+ * @head: head of the node
+ * Return: 0
  */
-
+void free_list(list_t *head)
 {
+	list_t *h;
+
+	if (head == 0)
+		return;
+	while (head != 0)
+	{
+		h = head;
+		head = head->next;
+		free(h->str);
+		free(h);
+	}
 }
