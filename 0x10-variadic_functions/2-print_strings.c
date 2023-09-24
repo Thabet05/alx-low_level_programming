00fdb68d044bedb1ad90b1e784974bc1ca7a4f17
Thabet05 <thabetkhelkhal@gmail.com>
2023-09-24 07:37:35 -0700

    Sun 24 Sep 2023 07:37:35 AM PDT

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -17,7 +17,8 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 		if (string)
 			printf("%s", string);
-		printf("(nil)");
+		else
+			printf("(nil)");
 		if (separator && i < (n - 1))
 			printf("%s", separator);
 	}
