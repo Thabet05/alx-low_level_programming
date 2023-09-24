aead6452cb44b80be17e71d6f94a231249828b15
Thabet05 <thabetkhelkhal@gmail.com>
2023-09-24 06:49:18 -0700

    Sun 24 Sep 2023 06:49:18 AM PDT

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,23 +8,22 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-        int i;
+	int i;
+	int j;
 
-        int j;
-
-        i = 0;
-        while (dest[i] != '\0')
-        {
-                i++;
-        }
-        j = 0;
-        while (j < n && src[j] != '\0')
-        {
-                dest[i] = src[j];
-                i++;
-                j++;
-        }
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
 	if (j == n)
 		dest[i] = '\0';
-        return (dest);
+	return (dest);
 }
