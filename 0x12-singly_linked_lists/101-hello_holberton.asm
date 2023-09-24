64dee5207b741ca5facc3f3275f9548470af3eae
Thabet05 <thabetkhelkhal@gmail.com>
2023-09-23 17:45:03 -0700

    Sat 23 Sep 2023 05:45:03 PM PDT

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,7 +1,9 @@
-#include "lists.h"
-/**
-*
-*/
-
-{
-}
+section		.text
+	extern	printf
+	global	start
+start:
+	mov	edi, msg
+	mov	eax, 0
+	call	printf
+section		.data
+	msg db "hello, holberton", 0xa, 0
