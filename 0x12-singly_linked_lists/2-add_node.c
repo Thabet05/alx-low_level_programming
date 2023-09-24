578c98046d4eedac34cfe174146a2fe15c7e47ce
Thabet05 <thabetkhelkhal@gmail.com>
2023-09-23 17:18:13 -0700

    Sat 23 Sep 2023 05:18:13 PM PDT

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,7 +1,20 @@
 #include "lists.h"
 /**
- *
+ * add_node - adds a new node at the beg of a list_t
+ * @head: first node of the old node* befor adding
+ * @str: the string of the new node
+ * Return: the address of a new node
  */
-
+list_t *add_node(list_t **head, const char *str)
 {
+	list_t *h;
+
+	h = malloc(sizeof(list_t));
+	if (h == 0)
+		return (0);
+	h->str = strdup(str);
+	h->len = strlen(str);
+	h->next = *head;
+	*head = h;
+	return (*head);
 }
