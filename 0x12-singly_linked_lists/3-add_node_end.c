70024ba090c257e80a96bc76e6c4c0cb66b380ce
Thabet05 <thabetkhelkhal@gmail.com>
2023-09-23 17:32:15 -0700

    Sat 23 Sep 2023 05:32:15 PM PDT

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * add_node - adds a new node at the end of a list_t
+ * add_node_end - adds a new node at the end of a list_t
  * @head: first node of the old node* befor adding
  * @str: the string of the new node
  * Return: the address of a new node
@@ -9,11 +9,11 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *h, *k;
 
-        h = malloc(sizeof(list_t));
-        if (h == 0)
+	h = malloc(sizeof(list_t));
+	if (h == 0)
 		return (0);
-        h->str = strdup(str);
-        h->len = strlen(str);
+	h->str = strdup(str);
+	h->len = strlen(str);
 	if (*head == 0)
 	{
 		*head = h;
