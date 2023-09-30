abdb3f278109b5d7f4d51447369a2514df62f315
Thabet05 <thabetkhelkhal@gmail.com>
2023-09-30 12:40:07 +0100

    last changes

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,7 +1,20 @@
 #include "lists.h"
 /**
- *
+ * print_list - function that prints all the elements of a list_t list
+ * @str: a pointer to a string
+ * @len: size of the string
+ * @next: a pointer to the next element
+ * Return: 0
  */
-
+size_t print_list(const list_t *h)
 {
+	typedef struct list_s
+	{
+		char *str;
+		unsigned int len;
+		struct list_s *next;
+	} list_t;
+	if (str == 0)
+		printf("0");
+
 }
