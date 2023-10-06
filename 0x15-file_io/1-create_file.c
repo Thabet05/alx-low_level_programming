a355333526d8194a4d7649049f878e857ec5800b
Thabet05 <thabetkhelkhal@gmail.com>
2023-10-06 13:41:09 -0700

    Fri 06 Oct 2023 01:41:09 PM PDT

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,13 +7,12 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-        ssize_t option;
-        char *content;
+        ssize_t option = 0;
 
-        if (filename == 0)
-                return (-1);
-        option = open(filename, O_wDONLY | O_TRUNC, 0600);
-        if (option == -1)
+	if (filename == 0)
+		return (-1);
+	option = open(filename, O_WRONLY | O_TRUNC, 0600);
+	if (option == -1)
 	{
 		option = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 		if (option == -1)
@@ -21,9 +20,16 @@ int append_text_to_file(const char *filename, char *text_content)
 			return (-1);
 		}
 		if (text_content == 0)
-    
-                close(FD);
-                free(content);
-                return (0);
-        }
+		{
+			close(option);
+			return (1);
+		}
+		option = write (option, text_content, strlen(text_content));
+		if (option == -1)
+                {
+			close(option);
+                        return (-1);
+                }
+		close(option);
+		return (1);
 }
