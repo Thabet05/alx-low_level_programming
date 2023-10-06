ec1823e8f316a15f6fce23c1ee963b172407a1d4
Thabet05 <thabetkhelkhal@gmail.com>
2023-10-06 13:55:52 -0700

    Fri 06 Oct 2023 01:55:52 PM PDT

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * append_text_to_file - function that appends text at the end of a file
+ * create_file - function that appends text at the end of a file
  * @filename: the name of the file
  * @text_content: NULL terminated string to add at the end of the file
  * Return: 1 or -1
  */
-int append_text_to_file(const char *filename, char *text_content)
+int create_file(const char *filename, char *text_content)
 {
 	ssize_t option = 0;
 
