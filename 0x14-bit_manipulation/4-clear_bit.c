7b408b3fe7f718531a1778371668cac2770b2149
Thabet05 <thabetkhelkhal@gmail.com>
2023-10-26 14:49:53 -0700

    Thu 26 Oct 2023 02:49:53 PM PDT

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,6 +1,22 @@
-#include <main.h>
+#include "main.h"
 /**
- * binary_to_uint - a function that converts a binary number to an unsigned int
- * @b: pointer to a string of 0 and 1 chars
- * Return: the converted number, or 0
+ * clear_bit - a function that sets the value of a bit to 0 at a given index
+ * @n: binnary number
+ * @index: is the index, starting from 0 of the bit you want to set
+ * Return: 
  */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int k = 1;
+
+	if (n == 0 || index > 63)
+		return (-1);
+	while (index > 0)
+	{
+		k = k << 1;
+		index--;
+	}
+	k = ~k;
+	*n = *n & k;
+	return (1);
+}
