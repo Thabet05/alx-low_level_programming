006118f789c2cc2d7dbef64facc1d16145c80008
Thabet05 <thabetkhelkhal@gmail.com>
2023-10-26 14:57:20 -0700

    Thu 26 Oct 2023 02:57:20 PM PDT

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,6 +1,35 @@
-#include <main.h>
+#include "main.h"
 /**
- * binary_to_uint - a function that converts a binary number to an unsigned int
- * @b: pointer to a string of 0 and 1 chars
- * Return: the converted number, or 0
+ * counting_ones - count the number of ones in the binary number
+ * @n: the input binnary number
+ * Return: the number of ones
  */
+int counting_ones(unsigned long int n)
+{
+	int t = 0;
+
+	while (n)
+	{
+		t += n & 1;
+		n >>= 1;
+	}
+	return (t);
+}
+/**
+ * flip_bits - returns the number of bits need to flip
+ * to get to another number
+ * @n: the 1st input number
+ * @m: the 2nd input number
+ * Return: number of bits
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int k;
+	int ind;
+
+	if (n == m || (m == 0 && n == 0))
+		return (0);
+	k = n ^ m;
+	ind = count_ones(k);
+	return (ind);
+}
