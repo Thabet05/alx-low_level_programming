e9dd8921d0cd9134af385ab43d9cea074e5bbbed
Thabet05 <thabetkhelkhal@gmail.com>
2023-10-26 11:07:40 -0700

    Thu 26 Oct 2023 11:07:40 AM PDT

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,6 @@
+#include <main.h>
+/**
+ * binary_to_uint - a function that converts a binary number to an unsigned int
+ * @b: pointer to a string of 0 and 1 chars
+ * Return: the converted number, or 0
+ */
