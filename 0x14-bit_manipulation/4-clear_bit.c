7758b0b87ce8e6648b67946c383444cd3ffd0481
Thabet05 <thabetkhelkhal@gmail.com>
2023-10-26 15:59:05 -0700

    Thu 26 Oct 2023 03:59:05 PM PDT

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -3,7 +3,7 @@
  * clear_bit - a function that sets the value of a bit to 0 at a given index
  * @n: binnary number
  * @index: is the index, starting from 0 of the bit you want to set
- * Return: 
+ * Return: 1 if it worked, or -1 if an error occurred
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
