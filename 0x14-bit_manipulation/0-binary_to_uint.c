793bc2836ea8b09d53b34d36c90c53ea2b8158bc
Thabet05 <thabetkhelkhal@gmail.com>
2023-10-26 13:43:18 -0700

    Thu 26 Oct 2023 01:43:18 PM PDT

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 /**
  * binary_to_uint - a function that converts a binary number to an unsigned int
  * @b: pointer to a string of 0 and 1 chars
@@ -6,9 +6,20 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int index;
+	int index1 = 0;
+	int index2 = 0;
+	int decimal = 0;
 
-	if (b == 0 | b[index] != 0 | b[index] != 1)
-		return (NULL);
-
+	if (b == 0)
+		return (0);
+	for (index1; index1 <= strlen(b); index1++)
+	{
+		if (b[index1] != 0 | b[index1] != 1)
+			return (0);
+	}
+	for (index2; index2 <= strlen(b); index2++)
+	{
+		 decimal += b[index2] * pow(2, index2);
+	}
+	return (decimal);
 }
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,7 +2,8 @@
 #define MAIN_H  
 #include <stdio.h> 
 #include <unistd.h>
-#include<string.h>
+#include <string.h>
+#include <math.h>
 
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
