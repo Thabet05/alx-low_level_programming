d7c83654c31c1c12c53f008f88c5e8aaf9d609d4
Thabet05 <thabetkhelkhal@gmail.com>
2023-10-26 11:11:21 -0700

    Thu 26 Oct 2023 11:11:21 AM PDT

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -4,3 +4,11 @@
  * @b: pointer to a string of 0 and 1 chars
  * Return: the converted number, or 0
  */
+unsigned int binary_to_uint(const char *b)
+{
+	int index;
+
+	if (b == 0 | b[index] != 0 | b[index] != 1)
+		return (NULL);
+
+}
