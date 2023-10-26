007cf89794fb7540b0d3352252868a7b1e6543d9
Thabet05 <thabetkhelkhal@gmail.com>
2023-10-26 16:03:05 -0700

    Thu 26 Oct 2023 04:03:05 PM PDT

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,13 +2,13 @@
 /**
  * binary_to_uint - a function that converts a binary number to an unsigned int
  * @b: pointer to a string of 0 and 1 chars
- * Return: the converted number, or 0
+ * Return: int number
  */
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int index1 = 0, decimal = 0;
 
-	if (b == 0)
+	if (b == NULL)
 		return (0);
 	while (b[index1] != 0)
 	{
