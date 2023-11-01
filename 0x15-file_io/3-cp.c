7a47838ec65c49ef153cf7461acb783d3e4b956a
Thabet05 <thabetkhelkhal@gmail.com>
2023-11-01 11:14:45 -0700

    Wed 01 Nov 2023 11:14:45 AM PDT

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *
+ */
+{
+	if()
+		{
+			exit(97);
+			write(STDOUT_FILENO, string, nmbrbytes);
+		}
+	fd = open(file_to, O_WRONLY | O_TRUNC);
+	if (fd = -1)
+	{
+		option = open(file_to, O_CREAT | O_WRONLY | O_TRUNC);
+		option = write(fd, content, nmbrbytes);
+		if (option == -1)
+		{
+			close(fd);
+		}
+	}
+}
